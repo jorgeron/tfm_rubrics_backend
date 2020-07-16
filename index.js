3e576c294eed95dee0ebe87efdadf12a61bdd8d7
Jorge Roncel <jorgeron1993@hotmail.com>
2020-07-16 09:02:32 +0200

    Adding gitignore

--- index.js
+++ index.js
@@ -1,7 +1,14 @@
 var express = require('express'),
     app = express(),
-    port = process.env.PORT || 8080;
+    port = process.env.PORT || 8080,
+    mongoose = require('mongoose'),
+    bodyParser = require('body-parser');
 
+
+app.use(bodyParser.urlencoded({
+    extended: true
+}));
+app.use(bodyParser.json());
 app.get("/", (req, res) => {
     res.json({ status: "success", message: "Welcome To Testing API" });
     });
